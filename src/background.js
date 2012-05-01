5d7c79f57d3a395a31a33f965bb67c538cf03fa9
samurai20000 <samurai20000___@___gmail.com>
2012-05-01 14:01:14 +0900

    add notification when user sends url to kindleit

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
 var BASE_URL = 'http://fivefilters.org/kindle-it/send.php?context=send';
+var notifier = new Notifier();
 
 var post_kindleit = function(url, email) {
     var page_url = url;
@@ -15,12 +16,18 @@ var post_kindleit = function(url, email) {
         url: BASE_URL,
         data: page_url_and_email_address,
         success: function(msg) {
-            // TODO
-            // 成功した際にレスポンスを返すか？
+            // TODO: 成功した時のアイコンを作成する
+            var icon        = chrome.extension.getURL('icon128.png');
+            var header      = 'Success';
+            var description = 'Success to send kindle using kindleit';
+            notifier.notify(icon, header, description);
         },
         error: function(XMLHttpRequest, textStatus, errorThrown){
-            // TODO
-            // 失敗した際はなにか返すべき
+            // TODO: 失敗した時のアイコンを作成する
+            var icon        = chrome.extension.getURL('icon128.png');
+            var header      = 'Failed';
+            var description = 'Fail to send kindle using kindleit. Try later.';
+            notifier.notify(icon, header, description);
         }
     });
 };
@@ -35,9 +42,17 @@ var _domain_number = function(domain) {
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     if (localStorage.getItem('email')) {
+        // notify
+        var icon        = chrome.extension.getURL('icon128.png');
+        var header      = 'Send';
+        var description = 'Send kindle by using kindleit';
+        notifier.notify(icon, header, description);
+
+        // post
         var email = localStorage.getItem('email');
         post_kindleit(tab.url, email);
     } else {
+        // go to email setting page
         chrome.tabs.create({
             "url": chrome.extension.getURL("options.html"),
         });
